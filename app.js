e0ffdbf5667a4b2408e85bb1af6cfc8b01a2b990
mwenda90 <47474154+mwenda90@users.noreply.github.com>
2024-04-30 23:00:23 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
+// app.js
 
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://mwendastro:Kiunga90@cluster0.bebl6ff.mongodb.net/i', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const PORT = 3000;
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// models/Message.js
+
+
+const messageSchema = new mongoose.Schema({
+  user: String,
+  text: String,
+  timestamp: { type: Date, default: Date.now },
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
+
+app.use(express.static('public'));
